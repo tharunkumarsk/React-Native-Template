33fdd339f371e26ef584518efc532c3035a8af0e
Tharunkumar <tharunkumarsk@gmail.com>
2020-01-10 16:08:34 +0100

    Add some more colors

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,22 @@
-const tintColor = '#2f95dc';
+const tintColor = "#2f95dc";
 
 export default {
   tintColor,
-  tabIconDefault: '#ccc',
+  tabIconDefault: "#ccc",
   tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
+  tabBar: "#fefefe",
+  errorBackground: "red",
+  errorText: "#fff",
+  warningBackground: "#EAEB5E",
+  warningText: "#666804",
   noticeBackground: tintColor,
-  noticeText: '#fff',
+  noticeText: "#fff",
+  purple: "#292477",
+  gray: "#757575",
+  white: "#fff",
+  red: "#b71845",
+  orange: "#f26f28",
+  blue: "#4e4cb8",
+  lightPurp: "#7c53c3",
+  pink: "#b93fb3"
 };
